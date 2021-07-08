2919437ba4bddef59713d727e6db05775460024a
chenbin <13790432378@163.com>
2021-07-08 23:16:44 +0800

    1.树的前序遍历，中序遍历，后序遍历。

--- src/main/com/java/tree/Tree.java
+++ src/main/com/java/tree/Tree.java
@@ -10,13 +10,47 @@ public class Tree {
         TreeNode left,right;
     }
 
-    void traverse(TreeNode root) {
+    /**
+     * 先序遍历
+     * @param root
+     */
+    void preOrder(TreeNode root) {
         if (root == null) {
             return;
         }
         System.out.println("TreeNode value = " + root.val);
-        traverse(root.left);
-        traverse(root.right);
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    /**
+     * 中序遍历
+     * 左子树，根节点，右子树
+     * @param root
+     */
+    void inOrder(TreeNode root) {
+        if (root == null) {
+            //终止条件
+            return;
+        }
+        inOrder(root.left);
+        System.out.println("val=" + root.val);
+        inOrder(root.right);
+    }
+
+    /**
+     * 后序遍历
+     * 左子树，右子树，根节点
+     * @param root
+     */
+    void postOrder(TreeNode root) {
+        //终止条件
+        if (root == null) {
+            return;
+        }
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.println("val = " + root.val);
     }
 
     /**
