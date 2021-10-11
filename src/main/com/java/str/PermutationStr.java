d17e450fe624afa212e122efb353ffd96d5a79cc
chenbin <yeshen.chen@tuya.com>
2021-10-11 16:05:06 +0800

    1.leetCode 567 字符串的排列

--- src/main/com/java/str/PermutationStr.java
+++ src/main/com/java/str/PermutationStr.java
@@ -51,3 +51,23 @@ public class PermutationStr {
     }
 
 }
+/**
+ * 给你两个字符串s1和s2 ，写一个函数来判断 s2 是否包含 s1的排列。如果是，返回 true ；否则，返回 false 。
+ *
+ * 换句话说，s1 的排列之一是 s2 的 子串 。
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s1 = "ab" s2 = "eidbaooo"
+ * 输出：true
+ * 解释：s2 包含 s1 的排列之一 ("ba").
+ * 示例 2：
+ *
+ * 输入：s1= "ab" s2 = "eidboaoo"
+ * 输出：false
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutation-in-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
