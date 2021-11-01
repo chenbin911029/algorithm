725496d74237135a73e17940fee34ad786f2bdf4
chenbin <13790432378@163.com>
2021-11-01 23:02:41 +0800

    leet 198. 打家劫舍2，房屋排成一个圈

---
+++ src/main/com/java/dynamic/houserobber/Rob2Circle.java
@@ -0,0 +1,63 @@
+package main.com.java.dynamic.houserobber;
+
+/**
+ * leet 198. 打家劫舍2，房屋排成一个圈
+ */
+public class Rob2Circle {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        /**
+         * 围成一圈，第一和最后一家相连，相邻房屋不能抢
+         * 则取 0 到 n-2 最优值 v1；1 到 n-1的最优值 v2；
+         * res = Math.max(v1, v2);
+         */
+        return Math.max(rangeOfRob(nums, 0, n - 2 ), rangeOfRob(nums, 1, n -1));
+    }
+
+    public int rangeOfRob(int[] nums, int start, int end ) {
+        int dp_i_2 = 0, dp_i_1 = 0, dp_i = 0;
+        for (int i = end; i >= start; i--) {
+            dp_i = Math.max(dp_i_1, nums[i] + dp_i_2);
+            dp_i_2 = dp_i_1;
+            dp_i_1 = dp_i;
+        }
+        return dp_i;
+    }
+
+}
+/**
+ * leet 198. 打家劫舍2
+ * 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈 ，
+ * 这意味着第一个房屋和最后一个房屋是紧挨着的。
+ * 同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+ *
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,3,2]
+ * 输出：3
+ * 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+ * 示例 2：
+ *
+ * 输入：nums = [1,2,3,1]
+ * 输出：4
+ * 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+ *      偷窃到的最高金额 = 1 + 3 = 4 。
+ * 示例 3：
+ *
+ * 输入：nums = [0]
+ * 输出：0
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= nums.length <= 100
+ * 0 <= nums[i] <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/house-robber-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
